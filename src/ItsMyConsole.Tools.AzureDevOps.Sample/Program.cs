df264703ca4aa27f072b87d9354162721ecf0eb0
dtarroz <damien.tarroz.apps@gmail.com>
2021-12-27 09:43:04 +0100

    fix(sample): change to net6.0

--- src/ItsMyConsole.Tools.AzureDevOps.Sample/Program.cs
+++ src/ItsMyConsole.Tools.AzureDevOps.Sample/Program.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿using ItsMyConsole;
+using ItsMyConsole.Tools.AzureDevOps;
+using System;
 using System.Threading.Tasks;
 
-namespace ItsMyConsole.Tools.AzureDevOps.Sample
+// ReSharper disable once CheckNamespace
+namespace MyExampleConsole
 {
     class Program
     {
         static async Task Main() {
             ConsoleCommandLineInterpreter ccli = new ConsoleCommandLineInterpreter();
 
+            // Console configuration
             ccli.Configure(options => {
                 options.Prompt = ">> ";
                 options.LineBreakBetweenCommands = true;
@@ -15,15 +19,18 @@ namespace ItsMyConsole.Tools.AzureDevOps.Sample
                 options.TrimCommand = true;
             });
 
+            // Azure DevOps configuration
             ccli.AddAzureDevOpsServer(new AzureDevOpsServer {
                                           Name = "TEST",
                                           Url = "https://<SERVEUR>",
                                           PersonalAccessToken = "<TOKEN>"
                                       });
 
+            // Display the title of the workitem
+            // Example : wi 1234
             ccli.AddCommand("^wi [0-9]*$", async tools => {
                 int workItemId = Convert.ToInt32(tools.CommandArgs[1]);
-                WorkItem workItem = await tools.AzureDevOps("VAL").GetWorkItemAsync(workItemId);
+                WorkItem workItem = await tools.AzureDevOps("TEST").GetWorkItemAsync(workItemId);
                 Console.WriteLine($"WI {workItemId} - {workItem.Title}");
             });
 

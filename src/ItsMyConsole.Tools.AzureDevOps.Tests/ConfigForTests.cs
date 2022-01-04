e7b1d488bc300ae592aa60f83ea0a808e4d53983
dtarroz <damien.tarroz.apps@gmail.com>
2022-01-04 16:05:36 +0100

    tests: add ConfigForTests empty

---
+++ src/ItsMyConsole.Tools.AzureDevOps.Tests/ConfigForTests.cs
@@ -0,0 +1,40 @@
+﻿namespace ItsMyConsole.Tools.AzureDevOps.Tests;
+
+public static class ConfigForTests
+{
+    public static AzureDevOpsServer GetAzureDevOpsServer() {
+        return new AzureDevOpsServer {
+            Name = "<NAME>",
+            Url = "<URL>",
+            PersonalAccessToken = "<PERSONAL_ACCESS_TOKEN>"
+        };
+    }
+
+    public static string[] GetAreaPaths() {
+        return new[] { "<AreaPath_1>", "<AreaPath_2>" };
+    }
+
+    public static string GetTeamProject() {
+        return "<TeamProject>";
+    }
+
+    public static string[] GetIterationPathCurrents() {
+        return new[] { "<IterationPath_1>", "<IterationPath_2>" };
+    }
+
+    public static string[] GetStates() {
+        return new[] { "New", "Active" };
+    }
+
+    public static string[] GetWorkItemTypes() {
+        return new[] { "Feature", "Bug" };
+    }
+
+    public static string GetAssignedToDisplayName() {
+        return "<AssignedToDisplayName>";
+    }
+
+    public static string[] GetActivities() {
+        return new[] { "Development", "Design" };
+    }
+}

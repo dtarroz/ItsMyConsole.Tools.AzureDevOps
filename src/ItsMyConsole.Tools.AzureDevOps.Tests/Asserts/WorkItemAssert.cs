248429e049c67f4654377bdab179365475494f84
dtarroz <damien.tarroz.apps@gmail.com>
2022-03-14 00:09:43 +0100

    feat! WorkItemCreateFields for Create, and WorkItemUpdateFields for Update

--- src/ItsMyConsole.Tools.AzureDevOps.Tests/Asserts/WorkItemAssert.cs
+++ src/ItsMyConsole.Tools.AzureDevOps.Tests/Asserts/WorkItemAssert.cs
@@ -6,31 +6,31 @@ namespace ItsMyConsole.Tools.AzureDevOps.Tests.Asserts;
 
 public static class WorkItemAssert
 {
-    public static void CheckNew(WorkItemFields workItemFields, WorkItem workItem) {
+    public static void CheckCreate(WorkItemCreateFields workItemCreateFields, WorkItem workItem) {
         Assert.NotNull(workItem);
-        Assert.Matches($"^{ConfigForTests.ServerUrl}.*/_apis/wit/workItems/{workItem.Id}$", workItem.Url);
+        Assert.Matches($"^{ConfigForTests.ServerUrl}.*/[_]apis/wit/workItems/{workItem.Id}$", workItem.Url);
         Assert.True(workItem.Id > 0);
         CheckRelations(null, workItem);
         Assert.False(workItem.IsFixedInChangeset);
         Assert.Null(workItem.Tags);
-        EqualNew(workItemFields, workItem);
+        EqualNew(workItemCreateFields, workItem);
     }
 
-    private static void EqualNew(WorkItemFields workItemFields, WorkItem workItem) {
-        Assert.NotNull(workItemFields);
+    private static void EqualNew(WorkItemCreateFields workItemCreateFields, WorkItem workItem) {
+        Assert.NotNull(workItemCreateFields);
         Assert.NotNull(workItem);
-        Assert.Equal(NullToDefault(workItemFields.AreaPath, ConfigForTests.AreaPathDefault), workItem.AreaPath);
-        Assert.Equal(workItemFields.TeamProject, workItem.TeamProject);
-        Assert.Equal(NullToDefault(workItemFields.IterationPath, ConfigForTests.IterationPathDefault), workItem.IterationPath);
-        Assert.Equal(workItemFields.Title, workItem.Title);
-        Assert.Equal(NullToDefault(workItemFields.State, ConfigForTests.StateDefault), workItem.State);
-        Assert.Equal(workItemFields.WorkItemType, workItem.WorkItemType);
-        Assert.Equal(EmptyToNull(workItemFields.AssignedToDisplayName), workItem.AssignedToDisplayName);
-        Assert.Equal(EmptyToNull(workItemFields.Activity), workItem.Activity);
-        Assert.Equal(EmptyToNull(workItemFields.Description), workItem.Description);
-        Assert.Equal(EmptyToNull(workItemFields.ReproSteps), workItem.ReproSteps);
-        Assert.Equal(EmptyToNull(workItemFields.SystemInfo), workItem.SystemInfo);
-        Assert.Equal(EmptyToNull(workItemFields.AcceptanceCriteria), workItem.AcceptanceCriteria);
+        Assert.Equal(NullToDefault(workItemCreateFields.AreaPath, ConfigForTests.AreaPathDefault), workItem.AreaPath);
+        Assert.Equal(workItemCreateFields.TeamProject, workItem.TeamProject);
+        Assert.Equal(NullToDefault(workItemCreateFields.IterationPath, ConfigForTests.IterationPathDefault), workItem.IterationPath);
+        Assert.Equal(workItemCreateFields.Title, workItem.Title);
+        Assert.Equal(NullToDefault(workItemCreateFields.State, ConfigForTests.StateDefault), workItem.State);
+        Assert.Equal(workItemCreateFields.WorkItemType, workItem.WorkItemType);
+        Assert.Equal(EmptyToNull(workItemCreateFields.AssignedToDisplayName), workItem.AssignedToDisplayName);
+        Assert.Equal(EmptyToNull(workItemCreateFields.Activity), workItem.Activity);
+        Assert.Equal(EmptyToNull(workItemCreateFields.Description), workItem.Description);
+        Assert.Equal(EmptyToNull(workItemCreateFields.ReproSteps), workItem.ReproSteps);
+        Assert.Equal(EmptyToNull(workItemCreateFields.SystemInfo), workItem.SystemInfo);
+        Assert.Equal(EmptyToNull(workItemCreateFields.AcceptanceCriteria), workItem.AcceptanceCriteria);
     }
 
     private static string EmptyToNull(string text) {
@@ -41,9 +41,9 @@ public static class WorkItemAssert
         return text ?? defaultText;
     }
 
-    public static void CheckUpdate(WorkItem workItem, WorkItemFields workItemFields, WorkItem workItemUpdate) {
+    public static void CheckUpdate(WorkItem workItem, WorkItemUpdateFields workItemUpdateFields, WorkItem workItemUpdate) {
         Assert.NotNull(workItem);
-        Assert.NotNull(workItemFields);
+        Assert.NotNull(workItemUpdateFields);
         Assert.NotNull(workItemUpdate);
         Assert.Equal(workItem.Url, workItemUpdate.Url);
         Assert.Equal(workItem.Id, workItemUpdate.Id);
@@ -52,38 +52,38 @@ public static class WorkItemAssert
         Assert.Equal(workItem.Related, workItemUpdate.Related);
         Assert.False(workItemUpdate.IsFixedInChangeset);
         Assert.Null(workItemUpdate.Tags);
-        WorkItemFields workItemFieldsUpdate = new WorkItemFields {
-            AreaPath = workItemFields.AreaPath ?? workItem.AreaPath,
-            TeamProject = workItemFields.TeamProject ?? workItem.TeamProject,
-            IterationPath = workItemFields.IterationPath ?? workItem.IterationPath,
-            Title = workItemFields.Title ?? workItem.Title,
-            State = workItemFields.State ?? workItem.State,
-            WorkItemType = workItemFields.WorkItemType ?? workItem.WorkItemType,
-            AssignedToDisplayName = workItemFields.AssignedToDisplayName ?? workItem.AssignedToDisplayName,
-            Activity = workItemFields.Activity ?? workItem.Activity,
-            Description = workItemFields.Description ?? workItem.Description,
-            ReproSteps = workItemFields.ReproSteps ?? workItem.ReproSteps,
-            SystemInfo = workItemFields.SystemInfo ?? workItem.SystemInfo,
-            AcceptanceCriteria = workItemFields.AcceptanceCriteria ?? workItem.AcceptanceCriteria
+        WorkItemUpdateFields workItemUpdateFieldsExpected = new WorkItemUpdateFields {
+            AreaPath = workItemUpdateFields.AreaPath ?? workItem.AreaPath,
+            TeamProject = workItemUpdateFields.TeamProject ?? workItem.TeamProject,
+            IterationPath = workItemUpdateFields.IterationPath ?? workItem.IterationPath,
+            Title = workItemUpdateFields.Title ?? workItem.Title,
+            State = workItemUpdateFields.State ?? workItem.State,
+            WorkItemType = workItemUpdateFields.WorkItemType ?? workItem.WorkItemType,
+            AssignedToDisplayName = workItemUpdateFields.AssignedToDisplayName ?? workItem.AssignedToDisplayName,
+            Activity = workItemUpdateFields.Activity ?? workItem.Activity,
+            Description = workItemUpdateFields.Description ?? workItem.Description,
+            ReproSteps = workItemUpdateFields.ReproSteps ?? workItem.ReproSteps,
+            SystemInfo = workItemUpdateFields.SystemInfo ?? workItem.SystemInfo,
+            AcceptanceCriteria = workItemUpdateFields.AcceptanceCriteria ?? workItem.AcceptanceCriteria
         };
-        EqualUpdate(workItemFieldsUpdate, workItemUpdate);
+        EqualUpdate(workItemUpdateFieldsExpected, workItemUpdate);
     }
 
-    private static void EqualUpdate(WorkItemFields workItemFields, WorkItem workItem) {
-        Assert.NotNull(workItemFields);
+    private static void EqualUpdate(WorkItemUpdateFields workItemUpdateFields, WorkItem workItem) {
+        Assert.NotNull(workItemUpdateFields);
         Assert.NotNull(workItem);
-        Assert.Equal(workItemFields.AreaPath, workItem.AreaPath);
-        Assert.Equal(workItemFields.TeamProject, workItem.TeamProject);
-        Assert.Equal(workItemFields.IterationPath, workItem.IterationPath);
-        Assert.Equal(workItemFields.Title, workItem.Title);
-        Assert.Equal(workItemFields.State, workItem.State);
-        Assert.Equal(workItemFields.WorkItemType, workItem.WorkItemType);
-        Assert.Equal(EmptyToNull(workItemFields.AssignedToDisplayName), workItem.AssignedToDisplayName);
-        Assert.Equal(EmptyToNull(workItemFields.Activity), workItem.Activity);
-        Assert.Equal(workItemFields.Description, workItem.Description);
-        Assert.Equal(workItemFields.ReproSteps, workItem.ReproSteps);
-        Assert.Equal(workItemFields.SystemInfo, workItem.SystemInfo);
-        Assert.Equal(workItemFields.AcceptanceCriteria, workItem.AcceptanceCriteria);
+        Assert.Equal(workItemUpdateFields.AreaPath, workItem.AreaPath);
+        Assert.Equal(workItemUpdateFields.TeamProject, workItem.TeamProject);
+        Assert.Equal(workItemUpdateFields.IterationPath, workItem.IterationPath);
+        Assert.Equal(workItemUpdateFields.Title, workItem.Title);
+        Assert.Equal(workItemUpdateFields.State, workItem.State);
+        Assert.Equal(workItemUpdateFields.WorkItemType, workItem.WorkItemType);
+        Assert.Equal(EmptyToNull(workItemUpdateFields.AssignedToDisplayName), workItem.AssignedToDisplayName);
+        Assert.Equal(EmptyToNull(workItemUpdateFields.Activity), workItem.Activity);
+        Assert.Equal(workItemUpdateFields.Description, workItem.Description);
+        Assert.Equal(workItemUpdateFields.ReproSteps, workItem.ReproSteps);
+        Assert.Equal(workItemUpdateFields.SystemInfo, workItem.SystemInfo);
+        Assert.Equal(workItemUpdateFields.AcceptanceCriteria, workItem.AcceptanceCriteria);
     }
 
     public static void CheckRelations(Dictionary<LinkType, List<int>> relations, WorkItem workItem) {
